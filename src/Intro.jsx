fb1b62bef1445740910449603e9ee7f2f11c1168
nobledsmarts <35113848+Nobledsmarts@users.noreply.github.com>
2022-06-26 09:24:02 +0100

    fix : root height. rename class to className in Intro.jsx

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -22,12 +22,12 @@ function Intro() {
             <div className="border-gray-200/75 border-[.01px] my-4"></div>
             <form>
                 <div className="p-2 flex justify-center">
-                    <label class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-yellow-700 hover:text-white">
-                        <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-yellow-700 hover:text-white">
+                        <svg className="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                             <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
                         </svg>
-                        <span class="mt-2 text-base leading-normal">Select your picture</span>
-                        <input type='file' class="hidden" />
+                        <span className="mt-2 text-base leading-normal">Select your picture</span>
+                        <input type='file' className="hidden" />
                     </label>
                 </div>
                 <div className="p-2">
